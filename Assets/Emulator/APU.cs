df9e1b30bc12633af876e101ae369b3bb987dde7
Aaron Myles Landwehr <snaphat@gmail.com>
2023-07-08 01:44:46 -0400

    Add initial ChatGPT APU code

---
+++ Assets/Emulator/APU.cs
@@ -0,0 +1,642 @@
+public class APU
+{
+    // Channels
+    private PulseChannel pulse1;
+    private PulseChannel pulse2;
+    private TriangleChannel triangle;
+    private NoiseChannel noise;
+    private DmcChannel dmc;
+
+    // Frame counter
+    private FrameCounter frameCounter;
+
+    public APU()
+    {
+        // Initialize the channels
+        pulse1 = new PulseChannel();
+        pulse2 = new PulseChannel();
+        triangle = new TriangleChannel();
+        noise = new NoiseChannel();
+        dmc = new DmcChannel();
+
+        // Initialize the frame counter
+        frameCounter = new FrameCounter();
+    }
+
+    public void Step()
+    {
+        // Increment the frame counter
+        frameCounter.Step();
+
+        // Update the channels based on the frame counter
+        pulse1.Step(frameCounter);
+        pulse2.Step(frameCounter);
+        triangle.Step(frameCounter);
+        noise.Step(frameCounter);
+        dmc.Step(frameCounter);
+    }
+
+    public void WriteRegister(ushort address, byte value)
+    {
+        switch (address)
+        {
+            case 0x4000:
+            case 0x4001:
+            case 0x4002:
+            case 0x4003:
+                pulse1.WriteRegister((ushort)(address - 0x4000), value);
+                break;
+            case 0x4004:
+            case 0x4005:
+            case 0x4006:
+            case 0x4007:
+                pulse2.WriteRegister((ushort)(address - 0x4004), value);
+                break;
+            case 0x4008:
+            case 0x4009:
+            case 0x400A:
+            case 0x400B:
+                triangle.WriteRegister((ushort)(address - 0x4008), value);
+                break;
+            case 0x400C:
+            case 0x400D:
+            case 0x400E:
+            case 0x400F:
+                noise.WriteRegister((ushort)(address - 0x400C), value);
+                break;
+            case 0x4010:
+            case 0x4011:
+            case 0x4012:
+            case 0x4013:
+                dmc.WriteRegister((ushort)(address - 0x4010), value);
+                break;
+            case 0x4015:
+                // TODO: Handle write to status register
+                break;
+            case 0x4017:
+                // TODO: Handle write to frame counter register
+                break;
+        }
+    }
+
+    public byte ReadRegister(ushort address)
+    {
+        byte value = 0;
+
+        switch (address)
+        {
+            case 0x4015:
+                // TODO: Handle read from status register
+                break;
+            default:
+                // Open bus: the most recent value put on the bus is returned
+                //value = openBus;
+                break;
+        }
+
+        return value;
+    }
+}
+
+public class PulseChannel
+{
+    private byte dutyCycle;
+    private byte volume;
+    private byte lengthCounter;
+    private ushort timer;
+    private ushort sweepShift;
+    private bool sweepNegate;
+    private ushort sweepPeriod;
+    private bool envelopeLoop;
+    private bool constantVolume;
+    private ushort envelopeDivider;
+
+    private byte sequenceStep;
+    private ushort frequency;
+
+    private int cycleCount;
+
+    private bool isEnabled;
+    private bool frameCounterMode;
+
+    public PulseChannel()
+    {
+        this.sequenceStep = 0;
+        this.cycleCount = 0;
+    }
+
+    public void Step(FrameCounter frameCounter)
+    {
+        // Pulse channel implementation
+    }
+
+    public void WriteRegister(int registerIndex, byte value)
+    {
+        switch (registerIndex)
+        {
+            case 0:
+                this.dutyCycle = (byte)(value >> 6);
+                this.volume = (byte)(value & 0x0F);
+                this.envelopeLoop = ((value & 0x20) != 0);
+                this.constantVolume = ((value & 0x10) != 0);
+                break;
+            case 1:
+                this.sweepShift = (ushort)(value & 0x07);
+                this.sweepNegate = ((value & 0x08) != 0);
+                this.sweepPeriod = (ushort)((value >> 4) & 0x07);
+                break;
+            case 2:
+                this.timer = (ushort)((this.timer & 0xFF00) | value);
+                this.frequency = (ushort)(1789773 / (16 * (this.timer + 1)));
+                break;
+            case 3:
+                this.lengthCounter = (byte)(value >> 3);
+                this.timer = (ushort)((this.timer & 0x00FF) | ((value & 0x07) << 8));
+                this.sequenceStep = 0; // Reset sequence on timer write
+                this.frequency = (ushort)(1789773 / (16 * (this.timer + 1)));
+                break;
+        }
+    }
+
+    public void SetEnable(bool isEnabled)
+    {
+        this.isEnabled = isEnabled;
+        if (!this.isEnabled)
+        {
+            this.lengthCounter = 0;
+        }
+    }
+
+    public void SetFrameCounterMode(bool mode)
+    {
+        this.frameCounterMode = mode;
+    }
+
+    public byte Output()
+    {
+        // If the channel is not enabled, output 0
+        if (!this.isEnabled)
+        {
+            return 0;
+        }
+
+        byte[] sequenceLookup = { 0b01000000, 0b01100000, 0b01111000, 0b10011111 };
+        byte sequence = sequenceLookup[this.dutyCycle];
+        byte output = (byte)((sequence >> (7 - this.sequenceStep)) & 1);
+        output *= this.volume;
+
+        // Move to the next step in the sequence every (timer + 1) APU cycles
+        this.cycleCount++;
+        if (this.cycleCount >= this.timer + 1)
+        {
+            this.cycleCount = 0;
+            this.sequenceStep = (byte)((this.sequenceStep + 1) % 8);
+        }
+
+        // Update the timer based on the sweep unit
+        if (this.sweepPeriod != 0 && this.sweepShift != 0 && this.cycleCount % (this.sweepPeriod * 2) == 0)
+        {
+            if (this.sweepNegate)
+            {
+                this.timer -= (ushort)(this.timer >> this.sweepShift);
+            }
+            else
+            {
+                this.timer += (ushort)(this.timer >> this.sweepShift);
+            }
+            this.frequency = (ushort)(1789773 / (16 * (this.timer + 1)));
+        }
+
+        // Update the volume based on the envelope
+        if (!this.constantVolume && this.cycleCount % 240 == 0) // APU frame counter runs at 60Hz
+        {
+            if (this.envelopeDivider == 0)
+            {
+                this.envelopeDivider = this.volume;
+                if (this.volume == 0)
+                {
+                    if (this.envelopeLoop)
+                    {
+                        this.volume = 0x0F;
+                    }
+                }
+                else
+                {
+                    this.volume--;
+                }
+            }
+            else
+            {
+                this.envelopeDivider--;
+            }
+        }
+
+        // Update the length counter
+        if (this.lengthCounter > 0 && this.cycleCount % (this.frameCounterMode ? 3728 : 29830) == 0) // APU frame counter runs at 240Hz or 192Hz depending on the mode
+        {
+            this.lengthCounter--;
+        }
+
+        return output;
+    }
+}
+
+public class TriangleChannel
+{
+    private const int TimerMin = 0x007;
+    private const int LengthCounterTableSize = 32;
+    private const int SequenceLength = 32;
+
+    private bool isEnabled;
+    private bool frameCounterMode;
+
+    private int timer;
+    private int timerReload;
+    private int sequencePos;
+    private int lengthCounter;
+    private int cycleCount;
+
+    private byte linearCounter;
+    private bool linearCounterReloadFlag;
+    private byte linearCounterReloadValue;
+
+    private byte[] lengthCounterTable = new byte[LengthCounterTableSize];
+    private byte[] triangleSequence = new byte[SequenceLength]
+    {
+        15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+    };
+
+    public TriangleChannel()
+    {
+        // Fill the length counter table
+        for (int i = 0; i < LengthCounterTableSize; i++)
+        {
+            lengthCounterTable[i] = (byte)(i * 2);
+        }
+    }
+
+    public void Step(FrameCounter frameCounter)
+    {
+        // Triangle channel implementation
+    }
+
+    public void WriteRegister(ushort address, byte data)
+    {
+        switch (address)
+        {
+            case 0x4008: // TRIANGLE_LINEAR_COUNTER
+                linearCounterReloadValue = (byte)(data & 0x7F);
+                linearCounterReloadFlag = (data & 0x80) != 0;
+                break;
+            case 0x4009: // Unused
+                break;
+            case 0x400A: // TRIANGLE_TIMER_LOW
+                this.timerReload = (this.timerReload & 0xFF00) | data;
+                break;
+            case 0x400B: // TRIANGLE_TIMER_HIGH
+                this.timerReload = (this.timerReload & 0x00FF) | (data << 8);
+                this.lengthCounter = lengthCounterTable[data >> 3];
+                break;
+        }
+    }
+
+    public void SetEnable(bool isEnabled)
+    {
+        this.isEnabled = isEnabled;
+        if (!this.isEnabled)
+        {
+            this.lengthCounter = 0;
+        }
+    }
+
+    public void SetFrameCounterMode(bool mode)
+    {
+        this.frameCounterMode = mode;
+    }
+
+    public void ClockLinearCounter()
+    {
+        if (linearCounterReloadFlag)
+        {
+            linearCounter = linearCounterReloadValue;
+        }
+        else if (linearCounter > 0)
+        {
+            linearCounter--;
+        }
+
+        if (!linearCounterReloadFlag)
+        {
+            linearCounterReloadFlag = false;
+        }
+    }
+
+    public byte Output()
+    {
+        // If the channel is not enabled or the linear counter is zero, output 0
+        if (!this.isEnabled || linearCounter == 0)
+        {
+            return 0;
+        }
+
+        byte output = 0;
+
+        if (this.timer < TimerMin)
+        {
+            this.timer = this.timerReload;
+            this.sequencePos = (this.sequencePos + 1) % SequenceLength;
+            output = triangleSequence[this.sequencePos];
+        }
+        else
+        {
+            this.timer--;
+        }
+
+        // Update the length counter
+        if (this.lengthCounter > 0 && this.cycleCount % (this.frameCounterMode ? 3728 : 29830) == 0) // APU frame counter runs at 240Hz or 192Hz depending on the mode
+        {
+            this.lengthCounter--;
+        }
+
+        return output;
+    }
+}
+
+public class NoiseChannel
+{
+    private const int LengthCounterTableSize = 32;
+    private bool isEnabled;
+    private bool frameCounterMode;
+
+    private int timer;
+    private int timerReload;
+    private int lengthCounter;
+    private int volume;
+
+    private byte envelope;
+    private bool envelopeStartFlag;
+    private bool constantVolumeFlag;
+    private int envelopeDivider;
+    private int envelopeDecayLevelCounter;
+
+    private int shiftRegister = 1;
+    private bool modeFlag;
+
+    private byte[] lengthCounterTable = new byte[LengthCounterTableSize];
+    private int[] noiseTimerPeriodTable = new int[16]
+    {
+        0x002, 0x004, 0x008, 0x010, 0x020, 0x030, 0x040, 0x050,
+        0x065, 0x07F, 0x0BE, 0x0FE, 0x17D, 0x1FC, 0x3F9, 0x7F2
+    };
+
+    public NoiseChannel()
+    {
+        // Fill the length counter table
+        for (int i = 0; i < LengthCounterTableSize; i++)
+        {
+            lengthCounterTable[i] = (byte)(i * 2);
+        }
+    }
+
+    public void Step(FrameCounter frameCounter)
+    {
+        // Noise channel implementation
+    }
+
+    public void WriteRegister(ushort address, byte data)
+    {
+        switch (address)
+        {
+            case 0x400C: // NOISE_VOLUME
+                constantVolumeFlag = (data & 0x10) != 0;
+                envelope = (byte)(data & 0x0F);
+                if (constantVolumeFlag)
+                {
+                    volume = envelope;
+                }
+                break;
+            case 0x400E: // NOISE_LOOPTIME
+                modeFlag = (data & 0x80) != 0;
+                timerReload = noiseTimerPeriodTable[data & 0x0F];
+                break;
+            case 0x400F: // NOISE_LENGTHCOUNTER
+                if (isEnabled)
+                {
+                    lengthCounter = lengthCounterTable[data >> 3];
+                }
+                envelopeStartFlag = true;
+                break;
+        }
+    }
+
+    public void SetEnable(bool isEnabled)
+    {
+        this.isEnabled = isEnabled;
+        if (!this.isEnabled)
+        {
+            this.lengthCounter = 0;
+        }
+    }
+
+    public void SetFrameCounterMode(bool mode)
+    {
+        this.frameCounterMode = mode;
+    }
+
+    public void ClockEnvelope()
+    {
+        if (envelopeStartFlag)
+        {
+            envelopeDecayLevelCounter = 15;
+            envelopeDivider = envelope;
+            envelopeStartFlag = false;
+        }
+        else if (envelopeDivider > 0)
+        {
+            envelopeDivider--;
+        }
+        else
+        {
+            envelopeDivider = envelope;
+            if (envelopeDecayLevelCounter > 0)
+            {
+                envelopeDecayLevelCounter--;
+            }
+            else if (envelopeDecayLevelCounter == 0 && constantVolumeFlag)
+            {
+                envelopeDecayLevelCounter = 15;
+            }
+        }
+        if (!constantVolumeFlag)
+        {
+            volume = envelopeDecayLevelCounter;
+        }
+    }
+
+    public byte Output()
+    {
+        // If the channel is not enabled, output 0
+        if (!this.isEnabled || this.lengthCounter == 0 || (this.shiftRegister & 1) == 1)
+        {
+            return 0;
+        }
+        return (byte)this.volume;
+    }
+
+    public void Clock()
+    {
+        if (timer == 0)
+        {
+            timer = timerReload;
+            int feedback = ((shiftRegister ^ (shiftRegister >> (modeFlag ? 6 : 1))) & 1);
+            shiftRegister = (shiftRegister >> 1) | (feedback << 14);
+        }
+        else
+        {
+            timer--;
+        }
+    }
+}
+
+public class DmcChannel
+{
+    private bool isEnabled;
+    private bool irqEnabled;
+    private bool loopFlag;
+    private int timer;
+    private int timerReload;
+    private int shiftRegister;
+    private int bitCounter;
+    private int sampleAddress;
+    private int sampleLength;
+    private int sampleCurrentAddress;
+    private int sampleCurrentLength;
+    private byte nextSample;
+    private bool sampleBufferEmpty = true;
+    private int[] dmcTimerPeriodTable = new int[16]
+    {
+        0x1AC, 0x17C, 0x154, 0x140, 0x11E, 0x0FE, 0x0E2, 0x0D6,
+        0x0BE, 0x0A0, 0x08E, 0x080, 0x06A, 0x054, 0x048, 0x036
+    };
+
+    public DmcChannel()
+    {
+    }
+
+    public void Step(FrameCounter frameCounter)
+    {
+        // DMC channel implementation
+    }
+
+    public void WriteRegister(ushort address, byte data)
+    {
+        switch (address)
+        {
+            case 0x4010: // DMC_FREQ
+                irqEnabled = (data & 0x80) != 0;
+                loopFlag = (data & 0x40) != 0;
+                timerReload = dmcTimerPeriodTable[data & 0x0F];
+                break;
+            case 0x4011: // DMC_RAW
+                shiftRegister = data & 0x7F;
+                break;
+            case 0x4012: // DMC_START
+                sampleAddress = 0xC000 | (data << 6);
+                break;
+            case 0x4013: // DMC_LEN
+                sampleLength = (data << 4) + 1;
+                break;
+        }
+    }
+
+    public void SetEnable(bool isEnabled)
+    {
+        this.isEnabled = isEnabled;
+        if (!this.isEnabled)
+        {
+            sampleCurrentLength = 0;
+        }
+        else if (sampleCurrentLength == 0)
+        {
+            Restart();
+        }
+    }
+
+    private void Restart()
+    {
+        sampleCurrentAddress = sampleAddress;
+        sampleCurrentLength = sampleLength;
+    }
+
+    public byte Output()
+    {
+        // If the channel is not enabled, output 0
+        if (!this.isEnabled || sampleBufferEmpty)
+        {
+            return 0;
+        }
+        return (byte)this.shiftRegister;
+    }
+
+    public void Clock()
+    {
+        if (timer == 0)
+        {
+            timer = timerReload;
+            if (bitCounter == 0)
+            {
+                bitCounter = 8;
+                if (!sampleBufferEmpty)
+                {
+                    shiftRegister = nextSample;
+                    sampleBufferEmpty = true;
+                }
+            }
+            if (bitCounter > 0)
+            {
+                shiftRegister = (shiftRegister >> 1) | ((nextSample & 1) << 7);
+                nextSample >>= 1;
+                bitCounter--;
+            }
+        }
+        else
+        {
+            timer--;
+        }
+
+        if (sampleBufferEmpty && sampleCurrentLength > 0)
+        {
+            // You need to replace this with the actual memory reading functionality.
+            nextSample = ReadMemory(sampleCurrentAddress);
+            sampleCurrentAddress = (sampleCurrentAddress + 1) & 0xFFFF;
+            sampleCurrentLength--;
+            sampleBufferEmpty = false;
+            if (sampleCurrentLength == 0)
+            {
+                if (loopFlag)
+                {
+                    Restart();
+                }
+                else if (irqEnabled)
+                {
+                    // Trigger IRQ
+                }
+            }
+        }
+    }
+
+    private byte ReadMemory(int address)
+    {
+        // Placeholder. Replace with actual memory read.
+        return 0;
+    }
+}
+
+public class FrameCounter
+{
+    public void Step()
+    {
+        // Frame counter implementation
+    }
+}
